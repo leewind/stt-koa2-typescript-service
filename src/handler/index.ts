a4836fb06743fd16eac48ac00b5c56fd29182acf
Michael.Lee <leewind19841209@gmail.com>
2017-07-27 14:22:12 +0800

    [dev]clean

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -9,15 +9,6 @@ import  {
   ReadFile 
 } from '../business/recognize'
 
-const RecognizeHandler = (ctx): void => {
-    ReadFile('test.wav').then((buffer: Buffer) => {
-        let recognizer = RecognizerSetup(RecognitionMode.Dictation, 'en-US', SpeechResultFormat['Simple'], 'eabdd9d57c334da2b7a06791157d2dd5', buffer);
-        RecognizerStart(recognizer).then( (result: string) => {
-            ctx.body = '{result: 1}';
-        });
-    })
-}
-
 export function recognize (ctx, next) {
     return next().then(() => {
         return ReadFile('test.wav').then((buffer: Buffer) => {
